1e72fc8b71e807c46a3618419a489495586a01cc
Zippy Speed <Zippy Speed@ZippySpeed-PC.gateway.pace.com>
2015-05-01 16:59:48 -0400

    Might need more work

--- Scheduling/src/scheduling/SchedulingGUI.java
+++ Scheduling/src/scheduling/SchedulingGUI.java
@@ -1,14 +1,14 @@
-import java.awt.Button;
-import java.awt.Frame;
-import java.awt.GridLayout;
-import javax.swing.JButton;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package scheduling;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 
 /**
  *
